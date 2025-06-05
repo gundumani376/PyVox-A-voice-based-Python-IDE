1b4a0a2ada5db7868d56a39abd93479311f65834
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-06-05 14:39:33 +0530

    commit

--- pyvox.py
+++ pyvox.py
@@ -1,7 +1,34 @@
 import tkinter as tk
-root=tk.Tk()
-root.title("PyVox")
-root.geometry("500x500")
-label = tk.Label(root, text="Hello, Tkinter!")
-label.pack(pady=20)
-root.mainloop()
+import time
+from tkinter import ttk
+
+def mainapp():
+    splash.destroy()
+    mainwindow= tk.Tk()
+    mainwindow.title("PyVox")
+    mainwindow.geometry("1100x800")
+
+    mainwindow.mainloop()
+
+splash = tk.Tk()
+splash.title("PyVox")
+windowwidth=675
+windowheight=672
+screenwidth = splash.winfo_screenwidth()
+screenheight = splash.winfo_screenheight()
+x=int((screenwidth/2) - (windowwidth/2))
+y=int((screenheight/2) - (windowheight/2))
+splash.geometry(f"{windowwidth}x{windowheight}+{x}+{y}")
+
+splash.overrideredirect(True)
+splashframe = ttk.Frame(splash)
+splashframe.pack(expand=True, fill=tk.BOTH)
+
+try:
+    banner_image = tk.PhotoImage(file="banner.png")
+    bannerlabel=tk.Label(splashframe, image=banner_image)
+    bannerlabel.pack(expand=True, fill=tk.BOTH)
+except: 
+    print("Error")
+splash.after(4000, mainapp)
+splash.mainloop()
